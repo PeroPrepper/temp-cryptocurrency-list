76b314b1ec0c0289ecc92b00ad6e45b17ddb368b
Peter Zlodej <peter.zlodej91@gmail.com>
2018-03-22 09:46:29 +0100

    Helper cells changed from class to functional components

--- src/screens/components/HelperCells.js
+++ src/screens/components/HelperCells.js
@@ -1,15 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
-class CellChange extends Component {
-  render() {
-    const { change } = this.props;
-
-    if (Number(change) >= 0) {
-      return <b style={{ color: "#009933" }}>{change}%</b>;
-    } else {
-      return <b style={{ color: "#ff0000" }}>{change}%</b>;
-    }
+export const CellChange = props => {
+  if (Number(props.change) >= 0) {
+    return <b style={{ color: "#009933" }}>{props.change}%</b>;
+  } else {
+    return <b style={{ color: "#ff0000" }}>{props.change}%</b>;
   }
-}
-
-export { CellChange };
+};
